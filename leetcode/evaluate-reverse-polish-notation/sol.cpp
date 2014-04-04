980024030cca2185a6e014d30a244c92262b484d
Hoyoung Jung <hyjung@nhn.com>
2014-04-04 12:51:57 +0900

    rev notation

--- leetcode/evaluate-reverse-polish-notation/sol.cpp
+++ leetcode/evaluate-reverse-polish-notation/sol.cpp
@@ -6,58 +6,60 @@
 using namespace std;
 
 class Solution {
-	public:
-		int evalRPN(vector<string> &tokens) {
-			vector<string> nums;
-			int ret;
-			while(!tokens.empty()) {
-				string str = pop(tokens);
-				if (str == "+" || str == "*" || str == "-" || str =="/") 
-					push(nums, calc(nums, str));
-				else
-					push(nums, str);
-			}
-			return atoi(pop(nums).c_str());
-		}
-	private:
-		void push(vector<string> &v, string str) {
-			v.insert(v.begin(), str);
-		}
-		string pop(vector<string> &v) {
-			string str = v.front();
-			v.erase(v.begin());
-			return str;
-		}
-		string calc(vector<string> &v, string op) {
-			int num2 = atoi(pop(v).c_str());
-			int num1 = atoi(pop(v).c_str());
-			int ret;
-			char c = op[0];
-			switch(c) {
-				case '+':
-					ret = num1 + num2;
-					break;
-				case '*':
-					ret = num1 * num2;
-					break;
-				case '/':
-					ret = num1 / num2;
-					break;
-				case '-':
-					ret = num1 - num2;
-					break;
-			}
-			ostringstream ssa;
-			ssa << ret;
-			return ssa.str();
-		}
+public:
+    int evalRPN(vector<string> &tokens) {
+        vector<string> nums;
+        for(vector<string>::iterator it=tokens.begin() ;it !=tokens.end(); it++) {
+            string str = *it;
+            if (str == "+" || str == "*" || str == "-" || str =="/")
+                CalcPush(nums, str);
+            else
+                Push(nums, str);
+        }
+        return ToInt(Pop(nums));
+    }
+    
+private:
+    inline int ToInt(string s) {return atoi(s.c_str());}
+    inline void Push(vector<string> &v, string str) {
+        v.insert(v.begin(), str);
+    }
+    inline string Pop(vector<string> &v) {
+        string str = v.front();
+        v.erase(v.begin());
+        return str;
+    }
+    void CalcPush(vector<string> &v, string op) {
+        int num2 = ToInt(Pop(v));
+        int num1 = ToInt(Pop(v));
+        int ret = 0;
+        char c = op[0];
+        switch(c) {
+            case '+':
+                ret = num1 + num2;
+                break;
+            case '*':
+                ret = num1 * num2;
+                break;
+            case '/':
+                ret = num1 / num2;
+                break;
+            case '-':
+                ret = num1 - num2;
+                break;
+        }
+        ostringstream ssa;
+        ssa << ret;
+        Push(v, ssa.str());
+    }
 };
 
-
+/* for test
 int main(int argc, char *argv[]) {
+    cout << "my program" << endl;
 	vector<string> token;
 	Solution s;
-
+    
 	while(1) {
 		string str;
 		cin >> str;
@@ -65,9 +67,10 @@ int main(int argc, char *argv[]) {
 			break;
 		token.push_back(str);
 	}
-
+    
 	int x = s.evalRPN(token);
 	cout << x << endl;
-
+    
 	return 0;
 }
+*/
