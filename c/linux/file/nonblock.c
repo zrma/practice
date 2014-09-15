e97d7478b0f3b0a8fed689451c8752f05263e9c8
Hoyoung Jung <k502000@naver.com>
2014-09-15 16:50:10 +0900

    0915 linux read ex

--- c/linux/file/nonblock.c
+++ c/linux/file/nonblock.c
@@ -2,11 +2,26 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <stdio.h>
 
-
 int main(int argc, char* argv[]) {
+	int fd, nr;
+	char buf[16];
+
+	fd = open(argv[1],O_RDONLY | O_NONBLOCK);
+	if (fd == -1)
+		perror("open");
+
+start:
+	nr = read(fd, buf, 16);
+	
+	if (nr == -1) {
+		if (errno == EAGAIN)
+			printf("EAGAIN\n");
+		perror("read");
+	}
 
 	return 0;
 }
