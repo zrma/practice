ef857728cd02297fa5eb9beeb1a314a369c69616
honux <honux@naver.com>
2014-09-17 23:35:42 +0900

    add makefile

--- c/linux/file/test.c
+++ c/linux/file/test.c
@@ -5,12 +5,17 @@
 #include <errno.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
 	int fd, nr;
 	char buf[16];
 
 	fd = open(argv[1],O_RDONLY | O_NONBLOCK);
+	if (argc != 2) {
+		printf("usage: %s filename\n", argv[0]);		
+		exit(1);
+	}
 	if (fd == -1)
 		perror("open");
 
@@ -22,7 +27,10 @@ start:
 			printf("EAGAIN\n");
 		perror("read");
 	}
-
+	if (nr > 0 ) {
+		write(1, buf, nr);
+		goto start;
+	}
 	close(fd);
 	return 0;
 }
