cc7c794b73db865b40984f7e5fcd4d447dde788c
Hoyoung Jung <k502000@naver.com>
2014-10-06 00:14:55 +0900

    add handler

--- node/simple_server/server.js
+++ node/simple_server/server.js
@@ -1,17 +1,19 @@
 var http = require('http');
-var url = require('utl');
+var url = require('url');
 
-function start() {
+function start(route, handle) {
 	function onReq(req, res) {
 		var pathname = url.parse(req.url).pathname;
 		console.log("Request for" + pathname +" recived: ");
+
+		route( handle, pathname);
+
 		res.writeHead(200, {"content-type": "text/plain" });
 		res.write("Hello world");
-		response.end();
+		res.end();
 	}
 	http.createServer(onReq).listen(8080);
 	console.log("Server has started");
-
 }
 
 exports.start = start;
