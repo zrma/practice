04f22530e3ee3e717dc3648c7aab2005395048fe
Hoyoung Jung <k502000@naver.com>
2014-10-05 11:32:37 +0900

    modify server.js - add url parsing

--- node/server.js
+++ node/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var url = require('utl');
 
 function start() {
 	function onReq(req, res) {
-		console.log("Req recived.");
+		var pathname = url.parse(req.url).pathname;
+		console.log("Request for" + pathname +" recived: ");
 		res.writeHead(200, {"content-type": "text/plain" });
 		res.write("Hello world");
 		response.end();
