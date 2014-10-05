e61170870dfe5263e875f96c03fa178e08fe5af3
Hoyoung Jung <k502000@naver.com>
2014-10-05 11:27:15 +0900

    simple http server

--- node/server.js
+++ node/server.js
@@ -1,5 +1,15 @@
 var http = require('http');
 
-fuction server() {
-	function onReq(
+function start() {
+	function onReq(req, res) {
+		console.log("Req recived.");
+		res.writeHead(200, {"content-type": "text/plain" });
+		res.write("Hello world");
+		response.end();
+	}
+	http.createServer(onReq).listen(8080);
+	console.log("Server has started");
+
 }
+
+exports.start = start;
