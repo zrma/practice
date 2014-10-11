22cbd053f92c52fa56c291392e8ae27148e37402
Hoyoung Jung <honux@naver.com>
2014-10-11 17:28:41 +0900

    node.js simple server exmple

--- node/simple_server/requestHandlers.js
+++ node/simple_server/requestHandlers.js
@@ -1,7 +1,8 @@
-var fs = require('fs');
-var querystring = require("querystring");
+var fs = require('fs'),
+	querystring = require("querystring"),
+	formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
 	console.log("Request handler 'start' was called.");
 	var body = fs.readFileSync('body.html', 'utf8');
 	response.writeHead(200, {"Content-Type": "text/html"});
@@ -9,23 +10,30 @@ function start(response, postData) {
 	response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: the text "+ 
-			querystring.parse(postData).text);
-	response.end();
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("parsing done");
+		fs.renameSync(files.upload.path, "/tmp/test.png");
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("receviced image: <br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
 }
 
-function show(response, postData) {
+function show(response) {
 	console.log("Request handler 'show' was called.");
-	fs.readFile("test.png","binary", function(err, file) {
+	fs.readFile("/tmp/test.png","binary", function(err, file) {
 		if (err) {
 			response.writeHead(500, {"Content-Type": "text/plain"});
 			response.write(err + "\n");
 			response.end();
 		} else {
-			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.writeHead(200, {"Content-Type": "image/png"});
 			response.write(file, "binary");
 			response.end();
 		}
--- node/simple_server/server.js
+++ node/simple_server/server.js
@@ -2,23 +2,12 @@ var http = require('http');
 var url = require('url');
 
 function start(route, handle) {
-	function onReq(request, response) {
-		var postData="";
+	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for" + pathname +" recived: ");
-
-		request.setEncoding("utf8");
-		request.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-			console.log("Recieved POST data chunk '" +
-				postDataChunk + "'");
-		});
-
-		request.addListener("end", function() {
-			route(handle, pathname, response, postData);
-		});
+		route(handle, pathname, response, request);
 	}
-	http.createServer(onReq).listen(8080);
+	http.createServer(onRequest).listen(8080);
 	console.log("Server has started");
 }
 
